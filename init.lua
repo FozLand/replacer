241beb6c84092cd7e8fada5112cbca6c8127b509
Foz <fozolo@gmail.com>
2016-05-27 19:47:06 -0400

    Add logging

--- init.lua
+++ init.lua
@@ -1,3 +1,4 @@
+local load_time_start = os.clock()
 
 replacer = {}
 
@@ -6,3 +7,10 @@ local modpath = minetest.get_modpath('replacer')
 dofile(modpath..'/inspect.lua')
 dofile(modpath..'/replacer.lua')
 
+minetest.log(
+	'action',
+	string.format(
+		'['..minetest.get_current_modname()..'] loaded in %.3fs',
+		os.clock() - load_time_start
+	)
+)
